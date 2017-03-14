22b36003350c34e16c8228585cb2726c9e7f1b04
严俊东 <1172482914@qq.com>
2017-03-14 15:56:53 +0800

    静态页面拆解完毕

--- src/components/search-all/SearchAllHead.vue
+++ src/components/search-all/SearchAllHead.vue
@@ -1,18 +1,100 @@
 <template>
+  <!--<div>-->
+    <!--<hr>-->
+    <!--搜索头部-->
+    <!--{{value}}-->
+    <!--<input ref="input" type="text" :value="value" @input="updateValue($event.target.value)">-->
+    <!--<a href="javascript:void(0)" @click="$router.go(-1)">取消</a>-->
+    <!--<div>-->
+      <!--<ul>-->
+        <!--<li v-for="item in games">-->
+          <!--<router-link to="" v-text="item.name"></router-link>-->
+        <!--</li>-->
+      <!--</ul>-->
+    <!--</div>-->
+    <!--<hr>-->
+  <!--</div>-->
   <div>
-    <hr>
-    搜索头部
-    {{value}}
-    <input ref="input" type="text" :value="value" @input="updateValue($event.target.value)">
-    <a href="javascript:void(0)" @click="$router.go(-1)">取消</a>
-    <div>
-      <ul>
-        <li v-for="item in games">
-          <router-link to="" v-text="item.name"></router-link>
-        </li>
-      </ul>
+    <div class="header pl-30 clearfix border-bottom">
+      <div class="header-l ps-r">
+        <input type="text" placeholder="请输入游戏名称或首字母" class="f30"/>
+      </div>
+      <h3 class="color-f7 f32 header-r">取消</h3>
     </div>
-    <hr>
+    <div class="rake pl-30 bg-fff dis-no"><!--删除dis-no就可以看见-->
+      <div class="rake-con clearfix border-bottom">
+        <a href="#" class="dis-b clearfix ps-r" >
+          <div class="rake-con-l  ps-a">
+            <i ></i>
+            <h4 class="f32 color-000 dis-in">地下城与勇士</h4>
+          </div>
+          <img src="../../assets/images/common/right.png" class="ps-a "/>
+        </a>
+      </div>
+      <div class="rake-con clearfix border-bottom">
+        <a href="#" class="dis-b clearfix ps-r" >
+          <div class="rake-con-l  ps-a">
+            <i ></i>
+            <h4 class="f32 color-000 dis-in">大天使之剑</h4>
+          </div>
+          <img src="../../assets/images/common/right.png" class="ps-a "/>
+        </a>
+      </div>
+      <div class="rake-con clearfix border-bottom">
+        <a href="#" class="dis-b clearfix ps-r" >
+          <div class="rake-con-l  ps-a">
+            <i ></i>
+            <h4 class="f32 color-000 dis-in">DATA2</h4>
+          </div>
+          <img src="../../assets/images/common/right.png" class="ps-a "/>
+        </a>
+      </div>
+      <div class="rake-con clearfix border-bottom">
+        <a href="#" class="dis-b clearfix ps-r" >
+          <div class="rake-con-l  ps-a">
+            <i class="ph"></i><!--................加了ph的是手游图标.................-->
+            <h4 class="f32 color-000 dis-in">大话西游(手游)</h4>
+          </div>
+          <img src="../../assets/images/common/right.png" class="ps-a "/>
+        </a>
+      </div>
+      <div class="rake-con clearfix border-bottom">
+        <a href="#" class="dis-b clearfix ps-r" >
+          <div class="rake-con-l  ps-a">
+            <i ></i>
+            <h4 class="f32 color-000 dis-in">大冲锋</h4>
+          </div>
+          <img src="../../assets/images/common/right.png" class="ps-a "/>
+        </a>
+      </div>
+      <div class="rake-con clearfix border-bottom">
+        <a href="#" class="dis-b clearfix ps-r" >
+          <div class="rake-con-l  ps-a">
+            <i ></i>
+            <h4 class="f32 color-000 dis-in">大唐无双零</h4>
+          </div>
+          <img src="../../assets/images/common/right.png" class="ps-a "/>
+        </a>
+      </div>
+      <div class="rake-con clearfix border-bottom">
+        <a href="#" class="dis-b clearfix ps-r" >
+          <div class="rake-con-l  ps-a">
+            <i ></i>
+            <h4 class="f32 color-000 dis-in">弹头奇兵</h4>
+          </div>
+          <img src="../../assets/images/common/right.png" class="ps-a "/>
+        </a>
+      </div>
+      <div class="rake-con clearfix border-bottom">
+        <a href="#" class="dis-b clearfix ps-r" >
+          <div class="rake-con-l  ps-a">
+            <i ></i>
+            <h4 class="f32 color-000 dis-in">斗破苍穹OL</h4>
+          </div>
+          <img src="../../assets/images/common/right.png" class="ps-a "/>
+        </a>
+      </div>
+    </div>
   </div>
 </template>
 
--- src/components/search-all/SearchAllHistory.vue
+++ src/components/search-all/SearchAllHistory.vue
@@ -1,16 +1,33 @@
 <template>
-  <div>
-    <div v-if="isShow">
-      历史记录
-      <ul>
-        <li v-for="item in games">
-          <router-link to="" v-text="item.name"></router-link>
-        </li>
-      </ul>
+  <!--<div>-->
+    <!--<div v-if="isShow">-->
+      <!--历史记录-->
+      <!--<ul>-->
+        <!--<li v-for="item in games">-->
+          <!--<router-link to="" v-text="item.name"></router-link>-->
+        <!--</li>-->
+      <!--</ul>-->
+    <!--</div>-->
+    <!--<button type="button" @click="isShow =! isShow">{{isShow? "点击隐藏": "点击显示"}}</button>-->
+    <!--<hr>-->
+  <!--</div>-->
+  <div class="pl-30 bg-fff mb-20 "><!--把dis-no删掉就可以看到-->
+    <div class="his clearfix border-bottom">
+      <input type="text" placeholder="搜索历史" class="f30 fl">
+      <div class="fr del">
+        <img src="../../assets/images/common/del.png" />
+      </div>
     </div>
-    <button type="button" @click="isShow =! isShow">{{isShow? "点击隐藏": "点击显示"}}</button>
-    <hr>
+    <div class="Warrior clearfix border-bottom">
+      <div class="Warrior-l fl">
+        <h3 class="color-000 f32 dis-in">地下城勇士</h3>
+      </div>
+      <div class="fr Warrior-r pr-30">
+        <img src="../../assets/images/common/right.png" />
+      </div>
+    </div>
   </div>
+
 </template>
 
 <script>

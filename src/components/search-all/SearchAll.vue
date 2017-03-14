1a20c90a63df377d6d88d5043e8f0fab0b0718f3
严俊东 <1172482914@qq.com>
2017-03-14 17:46:42 +0800

    热搜

--- src/components/search-all/SearchAll.vue
+++ src/components/search-all/SearchAll.vue
@@ -8,27 +8,54 @@
     <vue-history></vue-history>
 
     <!--.............................热门游戏......................-->
-    <vue-hot></vue-hot>
+    <vue-hot :hot-list="hotList"></vue-hot>
   </div>
 
 </template>
 
 <script>
-  import a from './data/index.js'
+  import http from './data/index.js'
   import vueHead from './SearchAllHead.vue'
   import vueHistory from './SearchAllHistory.vue'
   import vueHot from './SearchAllHot.vue'
-  console.log(a);
+
   export default {
+    name: "HomeSearch",
     data(){
       return {
-        searchInput: '',
+        searchInput: null,
+        hotList: null,
+        _num: null
       }
     },
     components: {
       vueHead,
       vueHistory,
       vueHot,
+    },
+    created(){
+      this.setNum();
+      this.setHostList();
+    },
+    methods: {
+      setHostList() {
+        http.getHot(this._num.hotNum).then((req) => {
+          this.hotList = req.list;
+          console.log('??');
+        }, () => {
+          console.log("数据请求错误");
+        })
+      },
+      setNum() {
+        let query = this.$route.query;
+        let num = {hotNum: 15, searchNum: 15, historyNum: 15};
+        for (let attr in query) {
+          if (num[attr]) {
+            num[attr] = Number(query[attr]) || 15;
+          }
+        }
+        this._num = num;
+      }
     }
   }
 </script>
--- src/components/search-all/SearchAllHot.vue
+++ src/components/search-all/SearchAllHot.vue
@@ -1,47 +1,55 @@
 <template>
   <!--<div>-->
-    <!--<div v-if="isShow">-->
-      <!--5173热搜-->
-      <!--<ul>-->
-        <!--<li v-for="item in games">-->
-          <!--<router-link to="" v-text="item.name"></router-link>-->
-        <!--</li>-->
-      <!--</ul>-->
-    <!--</div>-->
-    <!--<hr>-->
-    <div class="hot pl-30 bg-fff border-bottom ">
-      <h4 class="f30 color-999 border-bottom">热门游戏</h4>
-      <div class="game-n ps-r clearfix border-bottom">
-        <a class="fl f32 color-000" href="#">英雄联盟</a>
-        <i class="pa"></i>
-        <a class="fl f32 color-000" href="#"><span>王者荣耀(手游）</span></a>
-      </div>
-      <div class="game-n ps-r clearfix border-bottom">
-        <a class="fl f32 color-000" href="#">魔兽世界</a>
-        <i class="pa"></i>
-        <a class="fl f32 color-000" href="#"><span>梦幻西游(手游)</span></a>
-      </div>
-      <div class="game-n ps-r clearfix border-bottom">
-        <a class="fl f32 color-000" href="#">天涯明月刀</a>
-        <i class="pa"></i>
-        <a class="fl f32 color-000" href="#"><span>阴阳师(手游）</span></a>
-      </div>
-      <div class="game-n ps-r clearfix border-bottom">
-        <a class="fl f32 color-000" href="#">穿越火线</a>
-        <i class="pa"></i>
-        <a class="fl f32 color-000" href="#"><span>火线精英(手游）</span></a>
-      </div>
-      <div class="game-n ps-r clearfix border-bottom">
-        <a class="fl f32 color-000" href="#">疾风之刀</a>
-        <i class="pa"></i>
-        <a class="fl f32 color-000" href="#"><span>街篮(手游）</span></a>
-      </div>
-      <div class="game-n ps-r clearfix ">
-        <a class="fl f32 color-000" href="#">魔域</a>
-        <i class="pa"></i>
-        <a class="fl f32 color-000" href="#"><span>倩女幽魂(手游)</span></a>
-      </div>
+  <!--<div v-if="isShow">-->
+  <!--5173热搜-->
+  <!--<ul>-->
+  <!--<li v-for="item in games">-->
+  <!--<router-link to="" v-text="item.name"></router-link>-->
+  <!--</li>-->
+  <!--</ul>-->
+  <!--</div>-->
+  <!--<hr>-->
+  <div class="hot pl-30 bg-fff border-bottom ">
+    <h4 class="f30 color-999 border-bottom">5173热搜</h4>
+    <div v-for="item in showList" class="game-n ps-r clearfix border-bottom">
+      <!--<div  class="game-n ps-r clearfix border-bottom">-->
+      <a class="fl f32 color-000" href="#" v-text="item[0].name"></a>
+      <i class="pa"></i>
+      <a class="fl f32 color-000" href="#"><span v-text="item[1]?item[1].name:'' "></span></a>
     </div>
+
+    <!--<div class="game-n ps-r clearfix border-bottom">
+      <a class="fl f32 color-000" href="#">英雄联盟</a>
+      <i class="pa"></i>
+      <a class="fl f32 color-000" href="#"><span>王者荣耀(手游）</span></a>
+    </div>
+    <div class="game-n ps-r clearfix border-bottom">
+      <a class="fl f32 color-000" href="#">魔兽世界</a>
+      <i class="pa"></i>
+      <a class="fl f32 color-000" href="#"><span>梦幻西游(手游)</span></a>
+    </div>
+    <div class="game-n ps-r clearfix border-bottom">
+      <a class="fl f32 color-000" href="#">天涯明月刀</a>
+      <i class="pa"></i>
+      <a class="fl f32 color-000" href="#"><span>阴阳师(手游）</span></a>
+    </div>
+    <div class="game-n ps-r clearfix border-bottom">
+      <a class="fl f32 color-000" href="#">穿越火线</a>
+      <i class="pa"></i>
+      <a class="fl f32 color-000" href="#"><span>火线精英(手游）</span></a>
+    </div>
+    <div class="game-n ps-r clearfix border-bottom">
+      <a class="fl f32 color-000" href="#">疾风之刀</a>
+      <i class="pa"></i>
+      <a class="fl f32 color-000" href="#"><span>街篮(手游）</span></a>
+    </div>
+    <div class="game-n ps-r clearfix ">
+      <a class="fl f32 color-000" href="#">魔域</a>
+      <i class="pa"></i>
+      <a class="fl f32 color-000" href="#"><span>倩女幽魂(手游)</span></a>
+    </div>-->
+
+  </div>
   <!--</div>-->
 </template>
 
@@ -55,16 +63,27 @@
    */
   export  default {
     props: {
-      games: {
+      hotList: {
         type: Array,
         default(){
-          return [{name: "英雄联盟"}, {name: "英雄联盟"}];
+          return [];
         }
       }
     },
-    data() {
-      return {
-        isShow: true
+    computed: {
+      showList(){
+        // 如果hotList为空，返回[]
+        if (this.hotList == null) {
+          return [];
+        }
+        else {
+          let ret = [];
+          this.hotList.map((val, index) => {
+            if (index % 2 == 0) ret[index / 2] = [];
+            ret[parseInt(index / 2)].push(val);
+          });
+          return ret;
+        }
       }
     }
   }

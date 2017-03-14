dfb87477ba3479f4f5d7aca8a3c6bb74a5e85f66
严俊东 <1172482914@qq.com>
2017-03-14 15:22:01 +0800

    先写三个接口

--- src/components/search-all/data/index.js
+++ src/components/search-all/data/index.js
@@ -1,18 +1,11 @@
 /**
  * Created by 严俊东 on 2017/3/14.
  */
+// import res from './games.json'
 
-export default  {
-  /**
-   * 搜索返回数据
-   * @param num 数量
-   * @returns {Array} 返回一个游戏名数组
-   */
-  getSearch: function (num) {
 
-    return []
-  },
-  getHistory: function () {
+let getSearch = require('./getSearch.js');
+let getHistory = require('./getHistory.js');
+let getHot = require('./getHot.js');
 
-  },
-}
+export {getSearch, getHistory, getHot};
